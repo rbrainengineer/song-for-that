820559a288279dae7d6d9b26e84069fe7e0a44b7
Janice Wang <janicewang09@gmail.com>
2012-05-10 02:30:12 -0400

    adding full country lyric crawler and corpus

--- lyrics/lyrics/spiders/lyrics_spider.py
+++ lyrics/lyrics/spiders/lyrics_spider.py
@@ -1,5 +1,6 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
 import urllib
 import codecs
 import unicodedata
@@ -7,12 +8,24 @@ import unicodedata
 class LyricsSpider(BaseSpider):
     name = "lyrics"
     allowed_domains = ["cowboylyrics.com"]
-    start_urls = ["http://www.cowboylyrics.com/lyrics/swift-taylor/mine-30826.html"]
-
+    base_url = "http://www.cowboylyrics.com/lyrics/"
+    start_urls = [base_url+"lambert-miranda.html"]
+    dir_name = 'corpus/'
+    
     def parse(self, response):
+        
+        hxs = HtmlXPathSelector(response)
+        urls = hxs.select('//li/a/@href').extract()
+        for song_url in urls:
+            song_url = self.base_url+song_url
+            print song_url
+            request = Request(song_url, callback=self.parseLyricsPage)
+            yield request
+
+    def parseLyricsPage(self, response):
        hxs = HtmlXPathSelector(response)
        title = hxs.select('//title/text()').extract()[0]
-       filename = title+'.txt'
+       filename = self.dir_name + title+'.txt'
        f = codecs.open(filename, encoding='utf-8', mode='w')
        sites = hxs.select('//br')
        rootpath = "./";
